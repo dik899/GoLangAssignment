8fdc2b7914b317bd3cddd182885f6f80195f2c4b
dik899 <33979519+dik899@users.noreply.github.com>
2020-10-13 16:49:20 +0530

    Update dao_movies.go

--- dao/dao_movies.go
+++ dao/dao_movies.go
@@ -61,65 +61,46 @@ func (m *DAOMovies) Update(movie Movie) error {
 	return err
 }
 
-
+// Find a movie by its release year
 func (m *DAOMovies) FindByYear(release_year int) ([]Movie, error) {
-	log.Printf("inside findbyyear")
 	query := bson.M{"release_year": release_year}
-
-	// var movie Movie
 	movie := make([]Movie, 0, 10) 
 	err := db.C(COLLECTION).Find(query).Sort("title").All(&movie)
 	if err != nil {
 	    log.Fatal(err)
 	}
-
-	log.Print(movie)
 	return movie, err
 }
 
-func (m *DAOMovies) FindBelowRating(rating int) ([]Movie, error) {
-	log.Print("inside find below rating")
-	
+// Find a set of movies below a perticular rating
+func (m *DAOMovies) FindBelowRating(rating int) ([]Movie, error) {	
 	query :=bson.M{"rating": bson.M{"$gt": 0, "$lt": rating}}
-	
-  log.Print("query is ::",query)
-	// var movie Movie
 	movie := make([]Movie, 0, 10) 
 	err := db.C(COLLECTION).Find(query).Sort("rating").All(&movie)
 	if err != nil {
 	    log.Fatal(err)
 	}
-
-	log.Print(movie)
 	return movie, err
 }
-func (m *DAOMovies) FindAboveRating(rating int) ([]Movie, error) {
-	log.Print("inside find below rating")
-	
+
+// Find a set of movies above a perticular rating
+func (m *DAOMovies) FindAboveRating(rating int) ([]Movie, error) {	
 	query :=bson.M{"rating": bson.M{"$gt": rating}}
-	
-  log.Print("query is ::",query)
-	// var movie Movie
 	movie := make([]Movie, 0, 10) 
 	err := db.C(COLLECTION).Find(query).Sort("rating").All(&movie)
 	if err != nil {
 	    log.Fatal(err)
 	}
-
-	log.Print(movie)
 	return movie, err
 }
 
-
+// Find a set of movies upto perticular year
 func (m *DAOMovies) FindUptoYear(year int) ([]Movie, error) {
-	
 	query := bson.M{"release_year": bson.M{"$gt": 0, "$lt": year+1}}
 	movie := make([]Movie, 0, 10) 
 	err := db.C(COLLECTION).Find(query).Sort("release_year").All(&movie)
 	if err != nil {
 	    log.Fatal(err)
 	}
-
-	log.Print(movie)
 	return movie, err
 }
